e74819e2f9421ccf3e7c18fa7edd31c2f0e45a95
KovaaK <kovaak@gmail.com>
2015-09-21 22:15:56 -0400

    various improvements
    smoother color change, tick marks, etc

--- KovFPMtT.lua
+++ KovFPMtT.lua
@@ -10,42 +10,52 @@ registerWidget("KovFPMtT");
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
+
+local function avgColor(color1, color2, perc)
+	local r = (color1.r * perc) + (color2.r * (1-perc))
+	local g = (color1.g * perc) + (color2.g * (1-perc))
+	local b = (color1.b * perc) + (color2.b * (1-perc))
+	local a = (color1.a * perc) + (color2.a * (1-perc))
+	return Color(r, g, b, a)
+end
+
 function KovFPMtT:draw()
  
-    -- Early out if HUD shouldn't be shown.
-    if not shouldShowHUD() then return end;
+	-- Early out if HUD shouldn't be shown.
+	if not shouldShowHUD() then return end;
 	
 	-- Early out if not in 1v1 mode
 	if world.gameModeIndex ~= 2 then return end;
 
-    -- Find player 
-    local player = getPlayer()
+	-- Find player 
+	local player = getPlayer()
 
-    local fontSize = 40
-    local gaugeMaxFPM = 5
-	local rectHeight = 160
-	local rectWidth = 70
+	local fontSize = 40
+	local gaugeMaxFPM = 5
+	local rectHeight = 700
+	local rectWidth = 30
 
-    -- Colors
+	-- Colors
 	local transparency = 120
-    local textColor = Color(255,255,255,255);
+	local textColor = Color(255,255,255,255);
 
-    local BackgroundColor = Color(0,0,0,transparency);
-    local FarAhead = Color(60,80,255,255);
-    local CloseAhead = Color(0,170,0,255);
-    local CloseBehind = Color(255,255,0,255);
-    local FarBehind = Color(255,0,0,255);
+	local BackgroundColor = Color(0,0,0,transparency);
+	local FarAhead = Color(0,0,255,255); -- Blue
+	local BarelyAhead = Color(0,255,0,255); -- Green
+	local BarelyBehind = Color(255,255,0,255) -- Yellow
+	local FarBehind = Color(255,0,0,255); -- Red
    
-    local gaugeTickColor = Color(255,255,255,80);
+	local gaugeTickColor = Color(255,255,255,80);
 
-    -- Draw gauge background
-    nvgBeginPath();
+	-- Draw gauge background
+	nvgBeginPath();
 	nvgRect(-rectWidth/2,-rectHeight/2,rectWidth,rectHeight)
-    nvgFillColor(BackgroundColor);
-    nvgFill();
+	nvgFillColor(BackgroundColor);
+	nvgFill();
 
 	local myFrags = 0
 	local enemyFrags = 0
+
 	for p,player in pairs(players) do
 		if player.connected and player.state == PLAYER_STATE_INGAME then
 			if player == getPlayer() then
@@ -64,14 +74,12 @@ function KovFPMtT:draw()
 	FPM = math.max(FPM, -gaugeMaxFPM)
 	
 	local graphColor
-	if FPM > gaugeMaxFPM/2 then
-		graphColor = FarAhead
-	elseif FPM > 0 then
-		graphColor = CloseAhead
-	elseif FPM > -gaugeMaxFPM/2 then
-		graphColor = CloseBehind
+	if FPM > 0 then
+		local percentmix = (FPM)/(gaugeMaxFPM)
+		graphColor = avgColor(FarAhead,BarelyAhead,percentmix)
 	else
-		graphColor = FarBehind
+		local percentmix = (-FPM)/(gaugeMaxFPM)
+		graphColor = avgColor(FarBehind,BarelyBehind,percentmix)
 	end
 	
 	if world.gameState ~= 1 then -- only display when a real game is going on
@@ -79,32 +87,31 @@ function KovFPMtT:draw()
 	end
 	
 	-- Draw the first color
-    nvgBeginPath();
+	nvgBeginPath();
 	nvgRect(-rectWidth/2,0,rectWidth,-FPM/gaugeMaxFPM*rectHeight/2)
-    nvgFillColor(graphColor);
-    nvgFill();
+	nvgFillColor(graphColor);
+	nvgFill();
 	
 	--local tickMarkRange = 
-    -- Draw gauge tick line at the edge
-	--[[
-    for i=-rectHeight/2, rectHeight/2, rectHeight/10 do
-        nvgBeginPath();
+	-- Draw gauge tick line at the edge
+	----[[
+	for i=-rectHeight/2, rectHeight/2, rectHeight/10 do
+		nvgBeginPath();
 		nvgRect(-rectWidth/2+2,i,rectWidth-4,2)
-        nvgFillColor(gaugeTickColor);
-        nvgFill();
-    end
-	--]]
+		nvgFillColor(gaugeTickColor);
+		nvgFill();
+	end
+	----]]
 	
 	--[[
 	nvgFontSize(fontSize);
-    nvgFontFace("TitilliumWeb-Bold");
-    nvgTextAlign(NVG_ALIGN_CENTER, NVG_ALIGN_MIDDLE);
-    nvgFontBlur(0);
-    nvgFillColor(textColor);
-    nvgText(0, 0, world.gameState);
+	nvgFontFace("TitilliumWeb-Bold");
+	nvgTextAlign(NVG_ALIGN_CENTER, NVG_ALIGN_MIDDLE);
+	nvgFontBlur(0);
+	nvgFillColor(textColor);
+	nvgText(0, 0, FPM);
 	--]]
 
 
 
 end
-
